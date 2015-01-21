e1d220947e08befb306b14f5689b96a5d1273501
guillaumerodien <guillaume.rodien@bts-malraux.net>
2015-01-21 12:04:52 +0100

    version 1.0

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+double resultat operandeA=14 , operandeB=7 ;
+
+resultat = operandeA * operandeB;
+
+cin <<"Le resultat est"resultat<<endl; 
+
+resultat = operandeA / operandeB;
+
+cin <<"Le resultat est"resultat<<endl; 
+
+return 0;
+
